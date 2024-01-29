eeafc4ce6277e019b0d4f396d4704385ee47f54a
Pocsi Nandor <szilveszternandor@gmail.com>
2024-01-29 20:55:21 +0200

    Started navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Homepage from "./pages/Homepage";
+
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Homepage />
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <a href="#" className="nav-link">
+          Link
+        </a>
+        <h1>Navigation</h1>
+        <a href="#" className="nav-link">
+          Link
+        </a>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
